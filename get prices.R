b81220514a8f7c2035a5dc8337fcc9c8cb7a9f0c
Davi Stern <87588377+sterndavi@users.noreply.github.com>
2021-12-07 19:53:02 -0300

    Adicionado comentarios e pequenas otimizações

--- get prices.R
+++ get prices.R
@@ -2,82 +2,117 @@ library(rvest)
 library(tidyverse)
 library(here)
 library(lubridate)
-library(feather)
+library(rio)
+library(glue)
+
+
+###############################################################
+
 
 # lê arquivo com as urls
-df1 <- read_feather(path = paste(here(), "/urls.feather", sep = ""))
+df1 <- feather::read_feather(path = paste(here(), "/urls.feather", sep = ""))
 
-# transforma arquivo em lista
-# df1 <- as.list(df1)
 
 # função que vai iterar url por url criando uma lista para preços e nomes
 produtos <- function(urls){
+  
+  merc <- c() # prepara vetor de mercadorias
+  prec <- c() # prepara vetor de preços
+  
+  read_primato <- read_html(paste(urls)) # Lê url da lista
+    
+    Sys.sleep(runif(1,0,0.5)) # espera um valor aleatorio entre 0 e 0,5 segundos
+    
+    mercadoria_pn <- read_primato %>% 
+      html_nodes("a.link-nome-produto")%>%
+      html_text(trim = T) # Puxa todos os nomes das mercadorias da pagina
+    
+    merc = append(merc, mercadoria_pn) # Adiciona os nomes no vetor
+    
+    preco_pn <- read_primato %>%
+      html_nodes(".preco span") %>%
+      html_text2() # Puxa todos os preços da pagina
+    
+    prec <- append(prec, preco_pn) # Adiciona os preços no vetor
+    
+    
+    Sys.sleep(runif(1,0,0.5)) # Espera mais um tempo aleatorio entre 0 e 0,5
+    
+    
 
-  merc <- c()
-  prec <- c()
   
-  {read_primato <- read_html(paste(urls))
-  
-  Sys.sleep(runif(1,0,0.5))
-  
-  mercadoria_pn <- read_primato %>%
-    html_nodes("a.link-nome-produto")%>%
-    html_text(trim = T)
-  
-  merc = append(merc, mercadoria_pn)
-  
-  preco_pn <- read_primato %>%
-    html_nodes(".preco span") %>%
-    html_text2()
-  
-    prec <- append(prec, preco_pn)
-    
-    
-    Sys.sleep(runif(1,0,0.5))
-    
-   
-    }
-  
-return(data.frame(prec,merc))
+  return(data.frame(prec,merc)) # creio que parte do problema de listas dentro de listas
+                                # é causado por isso daqui, lembrar de corrigir quando der tempo
   
 }
 
-# chamar função para cada linha da dataframe
-get_prod <- map(df1$`.x[[i]]`[1:3], safely(produtos))
+
+# Chamar função para cada linha da dataframe
+
+get_prod <- map(df1$`.x[[i]]`, safely(produtos)) # Creio que a coluna precisa ser nomeada dessa forma por
+                                                  # se tratar de um elemento que era uma list
 
 
+# Retira um nivel da lista
+# Remove as mensagens de erro
 
-# retira um nivel da lista
-get_prod <- rlist::list.flatten(get_prod)
+get_prod <- get_prod %>%
+  rlist::list.flatten()%>%
+  rlist::list.remove(range = c("error.call", "error.message"))
 
-# remove as mensagens de erro
-get_prod <- rlist::list.remove(get_prod, range = c("error.call", "error.message"))
 
-# reagrupa os elementos da lista
-get_prod <- tapply(unlist(get_prod, use.names = FALSE), rep(names(get_prod), lengths(get_prod)), FUN = c)
+# Reagrupa os elementos da lista
 
-# transforma cada elemento em um DF
+get_prod <- tapply(
+  unlist(
+    get_prod, use.names = FALSE
+    ),
+  rep(
+    names(get_prod), lengths(get_prod)), FUN = c
+  ) # note to self: descobrir como essa parte funciona
+
+
+# Transforma cada elemento em um DF
+
 df_prods <- purrr::map(get_prod, as.data.frame)
 
-# une os DF em um só
+
+# Une os DF em um só
+
 df_prods <- as_tibble(df_prods)
+
+
+#faz o possível para transformar os dados em tidy
 
 df_prods <- df_prods %>%
   rename(
     'mercadorias' = result.merc,
     'precos' = result.prec
-    ) %>%
+  ) %>%     # Renomear as colunas
   mutate(
     precos = as.character(unlist(precos)),
     mercadorias = as.character(unlist(mercadorias)),
     "timestamp"  = lubridate::today()
-    )
+  )%>%      # Transformar os elementos de lista para caracteres
+  mutate(
+    precos = str_replace_all(precos, ",", ".")
+  )%>%      # Substituir "," por "." nos preços
+  mutate(
+    precos = parse_double(precos)
+  )         # Finalmente passar os preços de string para double
 
 
+# Gerar summario simples
+
+summ <- summary(df_prods)
+diag <- dlookr::diagnose(df_prods)
 
 # salva arquivo do dia
-write_feather(dfprods, path = paste(here(),"/data/preços", today(), ".feather", sep = ""))
 
+export(summ, file = glue("{here()}/data/summary {today()}.csv"), format = "csv")
 
+export(df_prods, file = glue("{here()}/data/precos {today()}.feather"), format = "feather")
+
+export(diag, file = glue("{here()}/data/diag {today()}.csv"), format = "csv")
 
 
